cf278c831b4546e654aa9a35b984bc8cbb1b2b71
Joshua Strobl <joshua@stroblindustries.com>
2015-12-05 15:13:01 +0200

    Added LightTable editor installer script. Refined existing Go install script by making it more flexible and easier to maintain (using variables) and suppressing wget.

--- programming-languages/install-go.sh
+++ programming-languages/install-go.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 currentDir=$(pwd)
+goVer="1.5.2"
+goTarball="go${goVer}.linux-amd64.tar.gz"
 
-if ! [ -f /home/$USER/Downloads/go1.5.2.linux-amd64.tar.gz ]; then # If the go file doesnt exist
-    echo "Downloading Go v1.5.2 to $USER's Downloads directory"
-    cd /home/$USER/Downloads && wget https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz # Download
+if ! [ -f /home/$USER/Downloads/$goTarball ]; then # If the go file doesnt exist
+    echo "Downloading Go v${goVer} to $USER's Downloads directory"
+    cd /home/$USER/Downloads && wget -q https://storage.googleapis.com/golang/$goTarball # Download
 fi
 
 cd "$currentDir"
@@ -13,8 +15,8 @@ echo "Removing any existing Go install (needs root)."
 sudo rm -rf /usr/local/go # Remove go from /usr/local/go
 sudo rm -f /usr/bin/go /usr/bin/godoc /usr/bin/gofmt # Remove symlinks
 
-echo "Installing Go."
-sudo tar -C /usr/local -xzf /home/$USER/Downloads/go1.5.2.linux-amd64.tar.gz # Extract go
+echo "Installing Go v${goVer}"
+sudo tar -C /usr/local -xzf /home/$USER/Downloads/$goTarball # Extract go
 sudo ln -s /usr/local/go/bin/{go,godoc,gofmt} /usr/bin/ # Do symlinks
 
 gorootExists=$(grep -Fc "GOROOT" /home/$USER/.bashrc) # Get the number of instances of GOROOT
