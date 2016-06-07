3b2434a924b962f8af27a09b49c3914bb35c8c81
Michael Rutherford <michaellogan.rutherford@gmail.com>
2016-06-07 11:48:52 -0500

    Added git config to post-install script

--- post-install/michael-post-install.sh
+++ post-install/michael-post-install.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-# Upgrade everything
+# Upgrade
 sudo eopkg up -y
 
-# Install developer packages
+# Developer packages
 sudo eopkg it -c system.devel -y
 
-# Install my applications
-sudo eopkg it binutils clang gcc geany gedit-plugins ghex git glibc-devel htop libgtk-2-devel libgtk-3-devel libparted-devel kernel-headers nasm pkg-config yasm -y
+# Applications
+sudo eopkg it binutils clang gcc gedit-plugins ghex git glibc-devel htop libgtk-2-devel libgtk-3-devel libparted-devel kernel-headers nasm pkg-config yasm -y
 
-# Install Google Chrome
+# Google Chrome
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml &&
 sudo eopkg it google-chrome-*.eopkg && sudo rm  google-chrome-*.eopkg
 
-# Install ISO tools
+# ISO tools
 sudo eopkg it squashfs-tools syslinux libisoburn -y
-
-# Gsettings
-# Theme
-gsettings set org.gnome.desktop.wm.preferences theme 'Arc-Darker'
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Darker'
 
 # gedit
 gsettings set org.gnome.gedit.preferences.editor scheme 'solarized-dark'
@@ -28,5 +23,16 @@ gsettings set org.gnome.gedit.preferences.editor tabs-size "uint32 8"
 gsettings set org.gnome.gedit.preferences.editor auto-indent false
 gsettings set org.gnome.gedit.preferences.editor insert-spaces true
 
-# budgie
+# Budgie
 gsettings set com.evolve-os.budgie.panel dark-theme true
+
+# Git
+git config --global user.name "Michael Rutherford"
+git config --global user.email "michaellogan.rutherford@gmail.com"
+cd ~
+mkdir Projects
+cd Projects
+git clone https://github.com/michaelrutherford/solus-installer.git
+cd ..
+
+
